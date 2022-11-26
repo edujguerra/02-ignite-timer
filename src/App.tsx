b5263d5f1b3b406eb6e96b9eeda25acdb16b83c1
Eduardo J. Guerra <edujguerra@gmail.com>
2022-11-26 19:34:19 -0300

    2211261934

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant='primary'></Button>
       <Button variant='secondary'></Button>
       <Button variant='success'></Button>
       <Button variant='danger'></Button>
       <Button></Button>
-    </>
+    </ThemeProvider>
   )
 }
