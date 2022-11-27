c3c5739bf45f30d4d3a95e28bbb68aa1eb4c8a5a
Eduardo J. Guerra <edujguerra@gmail.com>
2022-11-26 22:18:46 -0300

    2211262218

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"></Button>
-      <Button variant="secondary"></Button>
-      <Button variant="success"></Button>
-      <Button variant="danger"></Button>
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
